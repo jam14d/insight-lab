ae38a43cf4a37420f167bc29e709822345844ca2
jmortelrb <jmortel@revealbio.com>
2025-06-24 19:44:23 -0700

    title

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 # Title
-st.title("Insight Lab: Interactive Data Exploration")
+st.title("InsightLab: Interactive Data Exploration")
 
 # Sidebar Info
 st.sidebar.header("Data Analysis Reference")
